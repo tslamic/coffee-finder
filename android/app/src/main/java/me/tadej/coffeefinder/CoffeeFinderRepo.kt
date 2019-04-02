c0f39dfc60e26e4081c9482b09ad0b233fd7a723
Tadej <tadej.slamic@gmail.com>
2019-04-02 10:22:20 +0200

    Setting a stub deadline.

--- android/app/src/main/java/me/tadej/coffeefinder/CoffeeFinderRepo.kt
+++ android/app/src/main/java/me/tadej/coffeefinder/CoffeeFinderRepo.kt
@@ -3,7 +3,9 @@ package me.tadej.coffeefinder
 import androidx.annotation.WorkerThread
 import coffeefinder.CoffeeServiceGrpc
 import coffeefinder.Finder
+import io.grpc.Deadline
 import io.grpc.okhttp.OkHttpChannelBuilder
+import java.util.concurrent.TimeUnit
 
 class CoffeeFinderRepo : CoffeeFinderRepository {
     private val channel = OkHttpChannelBuilder
@@ -18,7 +20,8 @@ class CoffeeFinderRepo : CoffeeFinderRepository {
             .setLat(lat)
             .setLng(lng)
             .build()
-        val stub = CoffeeServiceGrpc.newBlockingStub(channel)
+        val deadline = Deadline.after(3, TimeUnit.SECONDS)
+        val stub = CoffeeServiceGrpc.newBlockingStub(channel).withDeadline(deadline)
         return stub.coffeeShops(point)
             .asSequence()
             .toList()
